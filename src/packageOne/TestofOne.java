d42869220e480a191f2abefb1a0a20c9ab40f55c
shankar4154 <shankar4154@gmail.com>
2021-09-08 20:17:36 +0100

    v1.2 of officiallaptop

--- src/packageOne/TestofOne.java
+++ src/packageOne/TestofOne.java
@@ -5,6 +5,7 @@ public class TestofOne {
 	public static void main(String[] args) {
 		
 		System.out.println("this is test1");
+		System.out.println("this is test 2 from official laptop");
 		
 		
 		
